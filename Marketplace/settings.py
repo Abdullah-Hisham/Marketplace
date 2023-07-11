c671bf3db0b8268fce59bdcacdd81fc98b5cf165
Abdullah_Hisham <abdulahhisham15@gmail.com>
2023-07-11 17:25:36 +0300

    001

--- Marketplace/settings.py
+++ Marketplace/settings.py
@@ -81,10 +81,7 @@ WSGI_APPLICATION = 'Marketplace.wsgi.app'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
 }
 
 
